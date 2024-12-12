ce26123f43817f1b05d3f8847eb9406ab8580ec4
unknown <maksims.zujevs@gmail.com>
2024-12-12 13:13:49 +0200

    Pievienota datuma un laika funkcionalitāte

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
+import datetime
 def sveiciens(vards):
     return f"Čau, {vards}!"
+
+def datuma_paradisana():
+    return f"Šodien ir {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
 if __name__ == "__main__":
     lietotaja_vards = input("Ievadi savu vārdu: ")
     print(sveiciens(lietotaja_vards))
+    print(datuma_paradisana())
